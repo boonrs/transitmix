d6a2af1d1dcdee3869566fa4d86f0ae88c6ac220
Danny Whalen <daniel.r.whalen@gmail.com>
2014-04-21 14:01:21 -0700

    Annotate model [ci skip]

--- spec/models/line_spec.rb
+++ spec/models/line_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: lines
+#
+#  id          :uuid             not null, primary key
+#  properties  :text             default("{}"), not null
+#  coordinates :text             default("[[],[]]"), not null
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'spec_helper'
 
 describe Line do
