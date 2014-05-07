4db48bd6e5fd1f880f2111c003a5f82812793a40
Sam Hashemi <sam@samhashemi.com>
2014-05-07 11:26:06 -0700

    Switch to Google Geocoding. So much faster.

--- app/js/views/HomeView.js
+++ app/js/views/HomeView.js
@@ -17,21 +17,32 @@ app.HomeView = Backbone.View.extend({
     var city = $('.homeCity').html();
     if (!city) return;
 
-    var geocodeUrl = 'http://nominatim.openstreetmap.org/search?format=json&q=';
-    geocodeUrl += encodeURI(city);
+    // Use the Google Maps Geocoding API
+    var url = 'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+    encodeURI(city) + '&sensor=false&key=AIzaSyCcgZLlBTM4GY0RhkUjy4MDD9RaZ0zIoiY';
 
-    // TODO: Add a progress bar, so you know we're waiting for a server
-    // response from the geocoding service
+    $.get(url, function(response) {
+      if (response.results.length === 0) {
+        console.log('Unable to geocode city. Womp Womp.');
+        // TODO: Throw up an error in the UI
+      }
 
-    $.get(geocodeUrl, function(response) {
+      // Get the coordinates for the center of the city
+      var latlng = response.results[0].geometry.location;
+      var center = [latlng.lat, latlng.lng];
 
-      // TODO: Check for empty responses, and throw up an error
-
-      var best = response[0];
-      var center = [best.lat, best.lon];
+      // Get the city's name. In google maps this is called 'locality'
+      var name = city;
+      var components = response.results[0].address_components;
+      for (var i = 0; i < components.length; i++) {
+        if (_.contains(components[i].types, 'locality')) {
+          name = components[i].long_name;
+          break;
+        }
+      }
 
       var newMap = new app.Map({
-        name: city,
+        name: name,
         center: center,
         zoomLevel: 14,
       });
