b8046e106be578ee595a0c56d09d9d9adc5010d3
Sam Hashemi <sam@samhashemi.com>
2014-05-13 10:33:38 -0700

    Second take at a feeback view. A small, expandable pill that lives in the bottom left corner.

--- app/js/app.js
+++ app/js/app.js
@@ -10,5 +10,9 @@ $(document).ready(function() {
   var options = { tileLayer: { detectRetina: true } };
   app.leaflet = L.mapbox.map('map', 'codeforamerica.h6mlbj75', options);
   
+  // Feedback View that we want visible at all times
+  var feedbackView = new app.FeedbackView();
+  $('body').append(feedbackView.render().el);
+
   app.router = new app.Router();
 });
